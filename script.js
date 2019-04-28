986c27c22caefe83a2fc2224e95e0c20b5fdb585
ricky <oparaiskelechi@gmail.com>
2019-04-28 20:54:17 +0100

    fixed the js file

---
+++ script.js
@@ -0,0 +1,109 @@
+// Get the Dom elememnts
+const ROLLDICE = document.getElementById('rollDice');
+const GAMERESULT = document.getElementById('gameResult');
+const ERROR = document.getElementById('errorReport');
+
+//method to get the sum of all roll
+const rolls = (array)=> {
+	let score = [];
+	//validate the array to only contain numbers btw 1-6
+	for(let rollNumber of array) {
+		if(![1,2,3,4,5,6].includes(rollNumber) ){
+		return  'numbers btw 1 - 6 only';
+		} 
+	}
+
+	//check for rolls that are 1 or 6
+	array.forEach((element, index, arr)=> {
+		if(arr[index - 1] == 1) {
+			score.push(0);
+		}else if(arr[index - 1] == 6) {
+			score.push(arr[index] * 2);
+		}else {
+			score.push(arr[index]);
+		}
+	});
+
+
+	// return the sum of the total scores
+	return score.reduce((prevScore, CurrScore)=> prevScore + CurrScore);
+};
+
+
+
+//method to display the rolls 
+ROLLDICE.addEventListener('click', ()=> {
+	//validate the users input
+	let inputNumber = document.getElementById('inputNumber').value;
+
+	if(inputNumber.length < 3 || inputNumber.length > 6) {
+		ERROR.style.display = 'block';
+		ERROR.textContent = `your input length should be greater than 3 and less than 7`;
+		//setTimeout(()=> document.querySelector('.errorReport').remove(), 3000);  
+	} else {
+
+		//split the input to an array and convert it to array of numbers
+		inputNumber	= inputNumber.split('').map(num => parseInt(num));
+
+		//Display Result
+		GAMERESULT.textContent = rolls(inputNumber);
+		document.getElementById('inputNumber').value = '';
+		ERROR.textContent = '';
+	}
+
+
+})// Get the Dom elememnts
+const ROLLDICE = document.getElementById('rollDice');
+const GAMERESULT = document.getElementById('gameResult');
+const ERROR = document.getElementById('errorReport');
+
+//method to get the sum of all roll
+const rolls = (array)=> {
+	let score = [];
+	//validate the array to only contain numbers btw 1-6
+	for(let rollNumber of array) {
+		if(![1,2,3,4,5,6].includes(rollNumber) ){
+		return  'numbers btw 1 - 6 only';
+		} 
+	}
+
+	//check for rolls that are 1 or 6
+	array.forEach((element, index, arr)=> {
+		if(arr[index - 1] == 1) {
+			score.push(0);
+		}else if(arr[index - 1] == 6) {
+			score.push(arr[index] * 2);
+		}else {
+			score.push(arr[index]);
+		}
+	});
+
+
+	// return the sum of the total scores
+	return score.reduce((prevScore, CurrScore)=> prevScore + CurrScore);
+};
+
+
+
+//method to display the rolls 
+ROLLDICE.addEventListener('click', ()=> {
+	//validate the users input
+	let inputNumber = document.getElementById('inputNumber').value;
+
+	if(inputNumber.length < 3 || inputNumber.length > 6) {
+		ERROR.style.display = 'block';
+		ERROR.textContent = `your input length should be greater than 3 and less than 7`;
+		//setTimeout(()=> document.querySelector('.errorReport').remove(), 3000);  
+	} else {
+
+		//split the input to an array and convert it to array of numbers
+		inputNumber	= inputNumber.split('').map(num => parseInt(num));
+
+		//Display Result
+		GAMERESULT.textContent = rolls(inputNumber);
+		document.getElementById('inputNumber').value = '';
+		ERROR.textContent = '';
+	}
+
+
+})
